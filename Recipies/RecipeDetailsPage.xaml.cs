3f0e494d004c8e16d27440118030ef20203fa5a6
Dzhemile Bairaktar <djemile14@abv.bg>
2025-01-25 19:17:15 +0200

    Add files via upload

---
+++ Recipies/RecipeDetailsPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Xamarin.Forms;
+using System.Linq;
+using static Recipies.MainPage;
+
+namespace Recipies
+{
+    public partial class RecipeDetailsPage : ContentPage
+    {
+        public RecipeDetailsPage(Recipe selectedRecipe)
+        {
+            InitializeComponent();
+
+            if (selectedRecipe != null)
+            {
+                selectedRecipe.IngredientsString = string.Join("\n- ", selectedRecipe.Ingredients.Prepend("-"));
+                selectedRecipe.StepsString = string.Join("\n", selectedRecipe.Steps.Select(s => $"Step {s.Key}: {s.Value}"));
+
+                BindingContext = selectedRecipe;
+            }
+        }      
+    }
+}
